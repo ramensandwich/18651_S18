3b11055eb0424519edb340a232523cce67eae856
Sean Buckley <sbuckley12489@gmail.com>
2018-05-02 02:01:28 -0400

    Cleanup visualizer code and added points of interest markers

--- movement_visualizer.py
+++ movement_visualizer.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.image as mpimg
 
 COLORS = ['#FF0000', '#FF6600', '#FFFF00', '#00FF00', '#0000FF', '#8000FF', '#663300', '#FFFFFF', "#00FFFF"]
 
@@ -9,57 +10,37 @@ def main():
 
     pointsFile = open('/Users/Sean/Desktop/Classes/18651/Data/points.json')
     breakfastMovementFile = open('/Users/Sean/Desktop/Classes/18651/Data/breakfast.json')
+    campus_map = plt.imread('/Users/Sean/Desktop/Classes/18651/Data/campus_map.png')
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.axis([0, 83, 0, 55])
-    pointsData = json.load(pointsFile)["Points"]
+    ax1.imshow(campus_map, extent=[0, 83, 0, 55])
+    pointsData = json.load(pointsFile)
     movementData = json.load(breakfastMovementFile)
 
-    #Draw all the points of interest first
-    for chunk in pointsData:
-        ax1.scatter(int(chunk["X"]), int(chunk["Y"]), c='#000000')
-
     dataframe = pd.DataFrame(movementData["data"]).astype(int)
-    # x = dataframe["X"]
-    # y = dataframe["Y"]
+    pointsOfInterest = pd.DataFrame(pointsData["Points"]).astype(int)
 
-#c=df['color'].apply(lambda x: colors[x])
+    poiX = pointsOfInterest["X"]
+    poiY = pointsOfInterest["Y"].apply(lambda y : -y + 55)
+    ax1.scatter(poiX, poiY, c="#000000", marker="*")
 
+    #TODO; Replace 1000 with actual total time value
     for i in xrange(1000):
         chunk = dataframe[dataframe.Tick == (5400 + i)]
         x = chunk["X"]
-        y = chunk["Y"]
+        #y to row:
+        #row = -y + 55
+        y = chunk["Y"].apply(lambda y : -y + 55)
         colors = chunk["ID"].apply(lambda x : COLORS[x % 9])
-        ax1.scatter(x,y, alpha = 0.2, c=colors)
+        ax1.scatter(x,y, alpha = 0.3, c=colors)
         plt.pause(0.1)
         plt.clf()
         print(i)
         ax1 = fig.add_subplot(111)
-        ax1.axis([0, 83, 0, 55])
+        ax1.imshow(campus_map, extent=[0, 83, 0, 55])
+        ax1.scatter(poiX, poiY, c="#000000", marker="*")
 
-    # for i in xrange(1000):
-    #     customers = movementData[0 + 1000*i : 999 + 1000*i]
-    #     for customer in customers:
-    #         print(customer.values())
-
-    # for i in xrange(1000):
-    #     asdf = [x for x in movementData if x["Tick"] == 5400 + i]
-    #     for customer in asdf:
-    #         ax1.scatter(int(customer["X"]), int(customer["Y"]), c=COLORS[int(customer["ID"]) % 9], alpha = 0.2)
-
-    # for customer in movementData:
-    #     print("X: " + customer["X"] + " Y: " + customer["Y"] + " Tick: " + customer["Tick"] + " ID: " + customer["ID"])
-    #     ax1.scatter(int(customer["X"]), int(customer["Y"]), c=COLORS[int(customer["ID"]) % 9], alpha = 0.2)
-
-    #     if int(customer["ID"]) == 999:
-    #         #Reset the window 
-    #         plt.pause(1)
-    #         plt.clf()
-    #         ax1 = fig.add_subplot(111)
-    #         ax1.axis([0, 83, 0, 55])
-    #         for chunk in pointsData:
-    #             ax1.scatter(int(chunk["X"]), int(chunk["Y"]), c='#000000')
 
     plt.show()
 
